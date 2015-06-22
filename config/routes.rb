349ee6075ab547d9be3d7251cd093aed67cebfd4
Sho Kodama <madakodama.1go@gmail.com>
2015-06-22 13:54:37 +0900

    日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+     resources :yogas
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -11,7 +13,8 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
   # Example resource route (maps HTTP verbs to controller actions automatically):
-   resources :yogas
+
+
 
   # Example resource route with options:
   #   resources :products do
